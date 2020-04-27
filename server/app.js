4083f5769ce54d9cb4f4b3641398d312986c9461
Maximilian Bleier <maximilian.bleier75@gmail.com>
2020-04-27 11:17:52 +0200

    Better structure

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ var position = {
     y= 0
 }
 
+socketio.on("connection", socket =>{
+    socket.emit("position", position)
+});
+
 
 http.listen(3000, () =>{
     console.log("Listening at 3000...");
