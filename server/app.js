7538901fdacf274993145727b8afc91d63f6c3ef
nikolardlvc <nikola7@live.at>
2020-05-06 12:38:19 +0200

    server side programming

--- server/app.js
+++ server/app.js
@@ -1,17 +1,46 @@
 const express = require("express")();
-const http = require("http").Server(Express);
-const socketio = require("socket.io")(Http);
+const server = require("http").Server(express);
+const io = require("socket.io")(server);
 
-var position = {
-    x: 0,
-    y: 0
-}
+let MAX_PLAYERS = 4;
+var player_count = 0;
+var players = [];
 
-socketio.on("connection", socket =>{
-    socket.emit("position", position)
+io.on("connection", function(socket) { // neue Verbindung eines Clients 
+    socket.on("new player", () => {
+        player_count++;
+        players[socket.id] = socket; // json object mit position, color, ...???
+        /*
+        players[socket.id] = {
+            x: 200,
+            y: 200,
+            length: 4,
+            color: 0xFF0000 
+        }
+        */
+       playerInfo = {
+           name: "TestName",
+           color: 0xFF0000
+       }
+       
+       socket.emit("info", playerInfo)
+    });
+
+    socket.on('disconnect', () => {
+        player_count--;
+        players.splice(socket.id,1); 
+    });
+
+    socket.on("message", function(message) { // neue Nachricht
+        socket.broadcast.emit(message);
+    });
 });
 
+//senden der spieler an alle spieler
+setInterval(() => {
+    io.broadcast.emit("bc", players);
+},1000/60)
 
-http.listen(3000, () =>{
-    console.log("Listening at 3000...");
+server.listen(8000, function (){
+    console.log('Server running at http://localhost:8000');
 });
